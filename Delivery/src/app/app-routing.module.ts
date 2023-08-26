68a749e7ada2373d9290d25e3533ffc3bc7de330
JorgeFuentes13 <jo.fuentess@duocuc.cl>
2023-08-26 19:47:08 -0400

    Diseño de Css Y html Entrega

--- Delivery/src/app/app-routing.module.ts
+++ Delivery/src/app/app-routing.module.ts
@@ -10,8 +10,6 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login-register/login/login.module').then( m => m.LoginPageModule)
   },
-
-
 ];
 @NgModule({
   imports: [
