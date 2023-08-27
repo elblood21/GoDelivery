3c80f81aa9e8c192bb47ba150a454f2fd4d26603
Victor0491 <victorlsepulveda@hotmail.com>
2023-08-27 18:45:09 -0400

    botón "realizar retiro" y mensaje toast

--- Delivery/src/app/detalles-pedido/detalles-pedido.page.ts
+++ Delivery/src/app/detalles-pedido/detalles-pedido.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { NavController } from '@ionic/angular'; // Asegúrate de importar NavController
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-detalles-pedido',
@@ -11,20 +12,34 @@ export class DetallesPedidoPage implements OnInit {
   pedidoInfo: any = {}; // Almacena la información del pedido
   horaCarga: string = ''; // Almacena la hora de carga del pedido
 
-  constructor(private route: ActivatedRoute, public navCtrl: NavController) {}
+  constructor(
+    private route: ActivatedRoute,
+    private navCtrl: NavController, // Agrega NavController aquí
+    private toastController: ToastController
+  ) {}
 
   ngOnInit() {
     // Obtén los parámetros de la URL (si estás pasando información a través de la URL)
     this.route.queryParams.subscribe(params => {
       this.pedidoInfo = JSON.parse(params['pedidoInfo']);
-      const now = new Date();
-      this.horaCarga = now.toLocaleTimeString(); // Puedes ajustar el formato de la hora según tus preferencias
     });
+
+    // Obtén la hora actual
+    const now = new Date();
+    this.horaCarga = now.toLocaleTimeString(); // Puedes ajustar el formato de la hora según tus preferencias
+  }
+
+  async mostrarMensaje() {
+    const toast = await this.toastController.create({
+      message: 'Pedido cargado para retiro',
+      duration: 2000, // Duración en milisegundos
+      position: 'bottom' // Posición en la que aparecerá el mensaje
+    });
+    toast.present();
   }
 
   volverPaginaAnterior() {
     this.navCtrl.back();
   }
 }
-
 
