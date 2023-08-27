4f533497a5c89480b90cf63180c2cf9313b16511
Victor0491 <victorlsepulveda@hotmail.com>
2023-08-27 16:54:54 -0400

    botón "Volver"

--- Delivery/src/app/detalles-pedido/detalles-pedido.page.ts
+++ Delivery/src/app/detalles-pedido/detalles-pedido.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-detalles-pedido',
@@ -9,13 +10,17 @@ import { ActivatedRoute } from '@angular/router';
 export class DetallesPedidoPage implements OnInit {
   pedidoInfo: any = {}; // Almacena la información del pedido
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, public navCtrl: NavController) {}
 
   ngOnInit() {
     // Obtén los parámetros de la URL (si estás pasando información a través de la URL)
     this.route.queryParams.subscribe(params => {
       this.pedidoInfo = JSON.parse(params['pedidoInfo']);
     });
+  }
+
+  volverPaginaAnterior() {
+    this.navCtrl.back();
   }
 }
 
