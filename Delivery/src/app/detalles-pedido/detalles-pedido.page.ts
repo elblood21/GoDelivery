f9310ad8cac4783b565db5b4191a77ea25e0d671
Victor0491 <victorlsepulveda@hotmail.com>
2023-08-27 16:38:42 -0400

    mejora botón abrir pedido

--- Delivery/src/app/detalles-pedido/detalles-pedido.page.ts
+++ Delivery/src/app/detalles-pedido/detalles-pedido.page.ts
@@ -19,3 +19,4 @@ export class DetallesPedidoPage implements OnInit {
   }
 }
 
+
--- Delivery/src/app/retiro/retiro.page.ts
+++ Delivery/src/app/retiro/retiro.page.ts
@@ -11,10 +11,23 @@ export class RetiroPage {
   constructor(public navCtrl: NavController) {}
 
   abrirDetallesPedido() {
-    // Código para redirigir a la página de detalles del pedido
+    const pedidoInfo = {
+      nombreTienda: 'PC Factory',
+      direccionRetiro: 'Dirección de retiro',
+      cliente: 'Nombre del cliente',
+      id: 'ID del pedido',
+      bultos: 'Número de bultos',
+    };
+  
+    this.navCtrl.navigateForward(['/detalles-pedido'], {
+      queryParams: {
+        pedidoInfo: JSON.stringify(pedidoInfo),
+      },
+    });
   }
 
   volverPaginaAnterior() {
     this.navCtrl.back();
   }
 }
+
