f26329f99faf040115769e1850c01d6c4e83d6a8
JorgeFuentes13 <jo.fuentess@duocuc.cl>
2023-09-01 15:53:53 -0400

    Button disable

--- Delivery/src/app/entrega/confirmacion-entrega/confirmacion-entrega.page.ts
+++ Delivery/src/app/entrega/confirmacion-entrega/confirmacion-entrega.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-confirmacion-entrega',
@@ -8,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
 export class ConfirmacionEntregaPage implements OnInit {
   rut: string = '';
   nombre: string = '';
+  confirmarHabilitado: boolean = false;
 
-  constructor() { }
+  constructor(public alertController: AlertController) { }
 
   ngOnInit() {
   }
 
+  verificarCamposCompletados() {
+    this.confirmarHabilitado = this.rut.trim() !== '' && this.nombre.trim() !== '';
+  }
+
+  async mostrarAlert() {
+    const alert = await this.alertController.create({
+      header: 'Confirmación',
+      message: '¿Estás seguro de que deseas confirmar?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Confirmación cancelada');
+          }
+        },
+        {
+          text: 'Confirmar',
+          handler: () => {
+            console.log('Confirmación exitosa');
+            // Aquí puedes agregar la lógica que se ejecutará cuando se confirme.
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
