3302f4de2752dbce9f62b769e29c08d47768be4d
Jordan Soto <jord.sotol@duocuc.cl>
2023-09-01 22:10:14 -0600

    Alerta Cierre sesión

--- Delivery/src/app/perfil/perfil.page.ts
+++ Delivery/src/app/perfil/perfil.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilPage implements OnInit {
 
-  constructor() { }
+  constructor(public alertControler: AlertController) { }
 
   ngOnInit() {
   }
 
+  async presentAlert(){
+    const alert = await this.alertControler.create({
+      header: 'Cerrar sesión',
+      message: '¿Esta seguro de querer cerrar la sesión?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: (blah) => {
+            console.log('Cancelar');
+          }
+        },
+        {
+          text: 'ok',
+          
+          handler: (blah) => {
+            console.log('Cerrar sesión');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
